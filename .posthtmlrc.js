8364bebaabc3a12a2cf31e1538ee2668e7d09ae1
Emiliano Pisu <pixu@EMILIANOPISUMAC.local>
2022-03-15 13:41:35 +0100

    feat(reborn): fixed locals for posthtml extend and include

--- .posthtmlrc.js
+++ .posthtmlrc.js
@@ -1,13 +1,15 @@
 const { locals } = require('./.mdblogrc.js');
 
+console.log(locals);
+
 module.exports = {
   "plugins": {
     "posthtml-doctype": {
       "doctype": "HTML 5"
     },
-    "posthtml-expressions": { locals: locals, removeScriptLocals: true },
-    "posthtml-extend": { root: './src', strict: false },
-    "posthtml-include": { root: './src' },
-    "posthtml-modules": {}
+    "posthtml-expressions": { locals, removeScriptLocals: true },
+    "posthtml-extend": { root: './src', strict: false, expressions: { locals } },
+    "posthtml-include": { root: './src', posthtmlExpressionsOptions: { locals } },
+    "posthtml-modules": { root: './src', locals }
   }
 }
