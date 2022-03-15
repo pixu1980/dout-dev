665587cc31533ce06c81db3551012814d5ddd205
Emiliano Pisu <pixu@EMILIANOPISUMAC.local>
2022-03-15 01:57:54 +0100

    feat(reborn): implemented mdMatter in mdblogrc

--- .mdblogrc.js
+++ .mdblogrc.js
@@ -1,16 +1,23 @@
 //requiring path and fs modules
 const path = require('path');
 const fs = require('fs');
+const matter = require('gray-matter');
+
 //joining path of directory 
 const dir = '/src/posts';
 const dirPath = path.join(__dirname, dir);
 const posts = [];
 
 fs.readdirSync(dirPath).forEach((file) => {
-  file.endsWith('.md') && posts.push({
-    name: file.split('.').slice(0, -1).join('.'),
-    path: [dir, file].join('/').replace('/src', '.'),
-  });
+  if (file.endsWith('.md')) {
+    const mdMatter = matter(fs.readFileSync('.' + [dir, file].join('/')));
+
+    posts.push({
+      ...mdMatter.data,
+      name: file.split('.').slice(0, -1).join('.'),
+      path: [dir, file].join('/').replace('/src', '.'),
+    });
+  }
 });
 
 const locals = {
