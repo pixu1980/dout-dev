8a711a37ba4ed0696a4984b2cf6fade8aa6e05df
Emiliano Pisu <pisuemiliano.1980@gmail.com>
2024-12-16 10:28:33 +0100

    feat(ui): restyling the ui according to latest indications

--- .posthtmlrc.js
+++ .posthtmlrc.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-const posts = require('./data/posts.json').map((post) => ({ ...post, date: new Date(post.date) ?? null }));
+const posts = require('./data/posts.json');
+const postsPublished = posts.filter((post) => post.published);
 
 const months = require('./data/months.json').map((month) => ({
   ...month,
@@ -12,16 +13,16 @@ const tags = require('./data/tags.json');
 
 const locals = {
   currentYear: new Date().getFullYear(),
-  lastPost: { ...posts[0], dateString: posts[0].date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }), tags: posts[0].tags.map((tag) => ({ ...tag, url: `./tags/${tag.key}.html` })) },
-  posts: posts,
-  months: months,
-  tags: tags,
+  lastPost: { ...postsPublished[0] },
+  posts: postsPublished,
+  months,
+  tags,
 };
 
 module.exports = {
   plugins: {
     'posthtml-expressions': { locals },
-    "posthtml-extend": { root: './src', strict: false, locals },
-    "posthtml-include": { root: './src', posthtmlExpressionsOptions: { locals } }
+    'posthtml-extend': { root: './src', strict: false, locals },
+    'posthtml-include': { root: './src', posthtmlExpressionsOptions: { locals } },
   },
 };
